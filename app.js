24d0d9e0b5abd156d299d66367e527f637355eab
Marty Miloslavich <mmilos3@uic.edu>
2021-04-28 20:13:57 +0000

    adding navbar and header photo

---
+++ app.js
@@ -0,0 +1,295 @@
+var bodyContainer = document.querySelector("#body-container");
+var body = document.querySelector("body");
+var mapDiv = document.createElement("div");
+    mapDiv.id = "map";
+// displays current date
+$(document).ready(function () {
+
+    $("#current-date").append("<strong>Today's Date:</strong>  " + (moment().format('dddd, MMMM Do')));
+
+    // fixes the enter button error where it reloads page
+    $(function () {
+        $("form").submit(function () {
+            return false;
+        });
+    });
+
+});
+
+
+// gets user's location
+$("#locationBtn").on("click", function (event) {
+    event.preventDefault();
+    // hides the map and review row until search button is clicked
+    if (event) {
+        $('#map-container').css("display", "initial");
+        $('#reviews-container').css("display", "initial");
+    }
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position);
+        geoLat = position.coords.latitude;
+        geoLon = position.coords.longitude;
+        console.log(geoLat, geoLon);
+
+        // loads user's location in google map
+        $("#reviews").empty();
+        var mapButton = document.querySelector("#map-nav-item");
+        mapButton.addEventListener("click", initialize());
+        //initialize();
+        // grabs weather for user's location
+        queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + geoLat + "&lon=" + geoLon + "&appid=8f775258afdec054195f89c38855f678&units=imperial";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            // console.log(response);
+            const geoCity = response.name;
+            // console.log(geoCity);
+            const country = response.sys.country;
+            // console.log(country);
+            var currentIcon = $("<img>")
+                .attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+            currentIcon.css({
+                "position": 'absolute'
+            });
+            console.log(currentIcon);
+            var rTemp0 = Math.floor(response.main.temp);
+            // console.log(rTemp0);
+
+            $("#current-city").empty();
+            $("#temp").empty();
+            $("#current-icon").empty();
+
+            $("#current-city").append("<strong>Current City:  </strong>" + geoCity + ", " + country);
+            $("#temp").append("<strong>Current Temp: </strong>" + rTemp0 + "° F");
+            $("#current-icon").append(currentIcon);
+        });
+    })
+});
+
+// grabbing user's inputted location
+var searchBtn = $("#searchBtn")
+
+searchBtn.on("click", function (event) {
+    event.preventDefault();
+    $("#reviews").empty();
+
+    // hides the map and review row until search button is clicked
+    if (event) {
+        $('#map-container').css("display", "initial");
+        $('#reviews-container').css("display", "initial");
+    }
+
+    var button = $(this);
+    console.log("click");
+
+    var city = $("#city-name").val().trim();
+    console.log(city);
+
+    // reverse lookup using open weather lol
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8f775258afdec054195f89c38855f678&units=imperial";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        const geoCity = response.name;
+        console.log(geoCity);
+        const country = response.sys.country;
+        console.log(country);
+        var currentIcon = $("<img>")
+            .attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+        // console.log(currentIcon);
+        // This click event is getting a different icon than the ready function
+        var rTemp0 = Math.floor(response.main.temp);
+        console.log(rTemp0);
+
+        geoLat = response.coord.lat
+        console.log(geoLat);
+        geoLon = response.coord.lon
+        console.log(geoLon);
+
+        initialize()
+
+        $("#temp").empty();
+        $("#current-icon").empty();
+        $("#current-city").empty();
+
+        $("#current-city").append("<strong>Current City:  </strong>" + geoCity + ", " + country);
+        $("#temp").prepend("<strong>Current Temp: </strong>" + rTemp0 + "° F");
+        $("#current-icon").prepend(currentIcon);
+    });
+});
+
+var aboutPageText = document.createElement("div");
+        aboutPageText.innerHTML = `<div>
+        <p style="color: white; "class="fs-5">This application was created by Martin Miloslavich. It allows a user to search for local coffee shops by city or by using their current location. It was built using Google Maps API, HTML5, CSS3, and Javascript. It uses Google Maps API, Google Places API, and Open Weather Map API.  
+        </p>
+        </div>`;
+
+//Displays the information about the application
+var aboutButton = document.querySelector("#about-nav-item");
+function displayAboutPage() {
+  removeAllChildNodes(bodyContainer);
+  if(body.contains(mapDiv)) {
+    body.removeChild(mapDiv);
+  }
+  activateNavItem(aboutButton);
+  bodyContainer.appendChild(aboutPageText);
+}
+aboutButton.addEventListener("click", displayAboutPage);
+
+// google maps
+let map;
+var infowindow;
+
+function initialize() {
+    // The location of user
+    var userLoc = {
+        lat: geoLat,
+        lng: geoLon
+    };
+    // The map, centered at location
+    map = new google.maps.Map(
+        document.getElementById('map'), {
+        zoom: 12,
+        center: userLoc
+    });
+
+    // The marker, positioned at user's location
+    var marker = new google.maps.Marker({
+        position: userLoc,
+        map: map,
+        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+    })
+
+    // info on marker
+    var infoWindow = new google.maps.InfoWindow({
+        content: '<p>You Are Here</p>'
+    });
+
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+    });
+    var request = {
+        location: userLoc,
+        radius: 8047,
+        types: ['cafe'] //This is what the cafe is searching for
+    };
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+};
+
+function callback(results, status) {
+    var cafeObjectArray = []
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var cafeObject = {
+                latitude: "",
+                longitude: "",
+                name: "",
+                rating: "",
+                place: ""
+            }
+            createMarker(results[i], i);
+            console.log(results[i], i);
+
+            cafeObject.latitude = results[i].geometry.location.lat();
+            cafeObject.longitude = results[i].geometry.location.lng();
+            cafeObject.name = results[i].name;
+            cafeObject.rating = results[i].rating;
+            cafeObject.place = results[i].place_id;
+            cafeObjectArray.push(cafeObject)
+        }
+        sortCafeObjectArray(cafeObjectArray);
+        pushCafeInfoToHTML(cafeObjectArray)
+    }
+}
+
+function pushCafeInfoToHTML(cafeObjectArray) {
+    var cardAction = $("#reviews");
+    for (i = 0; i < cafeObjectArray.length; i++) {
+      cardAction.append("<h6><strong>" + cafeObjectArray[i].name + "</strong></h6>");
+      cardAction.append("<p id='stars'>" + cafeObjectArray[i].rating + "  " + getStars(cafeObjectArray[i].rating) + "</p>");
+      cardAction.append("<p><a target='_blank' href = https://www.google.com/maps/search/?api=1&query=" + cafeObjectArray[i].latitude + "," + cafeObjectArray[i].longitude + ">" + "Directions" + "</a></p>");
+      cardAction.append("<p><a target='_blank' href = https://search.google.com/local/reviews?placeid=" + cafeObjectArray[i].place + ">" + "Reviews" + "</a></p>");
+      cardAction.append("<hr>");
+    }
+}
+
+// Sorting the objects by ratings of highest to lowest
+function sortCafeObjectArray(cafeObjectArray) {
+    cafeObjectArray.sort((a, b) => (a.rating > b.rating) ? -1 : 1)
+    return cafeObjectArray
+}
+
+// document.getElementById("stars").innerHTML = getStars(ratings);
+function getStars(ratings) {
+
+    // Round to nearest half
+    ratings = Math.round(ratings * 2) / 2;
+    let output = [];
+
+    // Append all the filled whole stars
+    for (var i = ratings; i >= 1; i--)
+        output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+    // If there is a half a star, append it
+    if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+    // Fill the empty stars
+    for (let i = (5 - ratings); i >= 1; i--)
+        output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+    return output.join('');
+
+}
+
+ //deletes all of the child nodes
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+//lists an item as active
+function activateNavItem(item) {
+  var navs = document.querySelectorAll(".nav-link");
+  item.classList.add("active");
+  navs.forEach(i => {
+    if(i != item) {
+      i.classList.remove("active");
+    }
+  });
+}
+
+function createMarker(place, index) {
+    var placeLoc = place.geometry.location;
+    var latti = place.geometry.location.lat();
+    var longi = place.geometry.location.lng();
+    console.log(latti, longi);
+    console.log(place.name);
+    var marker = new google.maps.Marker({
+        map: map,
+        position: {
+            lat: latti,
+            lng: longi
+        },
+        // icon: {
+        //     url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        //     // This marker is 20 pixels wide by 32 pixels high.
+        //     size: new google.maps.Size(20, 32),
+        //     // The origin for this image is (0, 0).
+        //     origin: new google.maps.Point(0, 0),
+        //     // The anchor for this image is the base of the flagpole at (0, 32).
+        //     anchor: new google.maps.Point(0, 32)
+        //   },
+        title: index + "",
+        zIndex: index,
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    })
+}
